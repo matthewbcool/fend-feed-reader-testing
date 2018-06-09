b27a48ff600cc76a99dd40d524752b2a69373599
Matt Cool <matthewbcool@gmail.com>
2018-06-09 10:13:52 -0500

    added url and name test suites

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -14,29 +14,31 @@ $(function() {
     * feeds definitions, the allFeeds variable in our application.
     */
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
+
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
+    });
+    describe('Url Feeds', function() {
+
+        it('are defined', function() {
+            expect(allUrls).toBeDefined();
+            expect(allUrls.length).not.toBe(0);
+        });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
+    });
+    describe('Name Feeds', function() {
+
+        it('are defined', function() {
+            expect(allNames).toBeDefined();
+            expect(allNames.length).not.toBe(0);
+        });
+
+
     });
 
 
--- js/app.js
+++ js/app.js
@@ -21,8 +21,26 @@ var allFeeds = [
     }, {
         name: 'Linear Digressions',
         url: 'http://feeds.feedburner.com/udacity-linear-digressions'
-    }
+    },
+
 ];
+
+/* This is a global variables I made to test, polluting the global namespace, 
+*  with all the Urls and names because Ive spent days on this project now and I cant 
+*  figure out how else to test if they are defined. 
+*/
+var allUrls = [];
+
+for (feed of allFeeds) {
+    allUrls.push(feed.url)
+};
+
+var allNames = [];
+
+for (feed of allFeeds) {
+    allNames.push(feed.name)
+};
+console.log(allNames)
 
 /* This function starts up our application. The Google Feed
  * Reader API is loaded asynchonously and will then call this
@@ -112,7 +130,6 @@ $(function() {
 
         feedId++;
     });
-
     /* When a link in our feedList is clicked on, we want to hide
      * the menu, load the feed, and prevent the default action
      * (following the link) from occurring.
